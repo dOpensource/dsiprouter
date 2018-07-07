d48e402887cc857ec39805dc425cbc4821f7070b
root <root@demo-dsiprouter.localdomain>
2018-07-07 13:47:26 +0000

    Adding install script for blockchain

--- gui/modules/blockchain/install.sh
+++ gui/modules/blockchain/install.sh
@@ -1,17 +1,70 @@
 #!/bin/bash
 #set -x
 ENABLED=1
+MODULEDIR=$PWD/gui/modules/blockchain
+GETHDATADIR=$MODULEDIR/gethDataDir
 
-function install {
+function createGenesisFile {
 
-if [ $ENABLED == "0" ];then
-    exit
+GENESISFILE=`cat <<EOF
+{
+    "config": {
+        "chainId": 88888,
+        "homesteadBlock": 0,
+        "eip155Block": 0,
+        "eip158Block": 0
+    },
+    "coinbase" : "0x0000000000000000000000000000000000000000",
+    "difficulty" : "0x1",
+    "extraData" : "0x00",
+    "gasLimit" : "8000000",
+    "nonce" : "0x0000000000000042",
+    "mixhash" : "0x0000000000000000000000000000000000000000000000000000000000000000",
+    "parentHash" : "0x0000000000000000000000000000000000000000000000000000000000000000",
+    "timestamp" : "0x00",
+    "alloc" : {
+        "$ACCOUNT": {"balance": "888888888888888888888888"}
+    }
+}
+EOF
+`
+echo $GENESISFILE > $GETHDATADIR/genesis.json
 
 
-    git clone https://github.com/ethereum/go-ethereum  
-    cd go-ethereum 
+}
 
-fi
+
+function init {
+
+    if [ -e $GETHDATADIR ]; then
+
+	    rm -rf  $GETHDATADIR
+    fi
+
+    mkdir $GETHDATADIR
+    cp $MODULEDIR/password.txt $GETHDATADIR
+    
+    #Download the geth docker image and generate an ethereum account
+    ACCOUNT=$(docker run -it -v $GETHDATADIR:/root/.ethereum ethereum/client-go:stable account new --password /root/.ethereum/password.txt | sed '1,2d')
+
+    #Parse out the account number
+    ACCOUNT=`echo $ACCOUNT | cut -d " " -f 2 |  awk '{print substr($0,2,length($0)-3)}'`
+    echo $ACCOUNT
+
+    #Generate a GenesisFile based on the Account Number
+    createGenesisFile
+
+    #Initialize the block chain
+    docker run -it -p 8545:8545 -p 30303:30303 -v $GETHDATADIR:/root/.ethereum ethereum/client-go:stable --rpc --rpcaddr "127.0.0.1" init /root/.ethereum/genesis.json
+
+
+
+}
+
+function startConsole {
+
+
+    docker run -it -p 8545:8545 -p 30303:30303 -v $GETHDATADIR:/root/.ethereum ethereum/client-go:stable --rpc --rpcaddr "127.0.0.1" --networkid $NETWORKID --nodiscover --verbosity 4 console
 
 }
 
@@ -21,6 +74,18 @@ function uninstall {
 echo ""
 
 }
+
+function install {
+
+if [ $ENABLED == "0" ];then
+    exit
+fi
+
+init
+NETWORKID=12345
+startConsole
+}
+
 
 # This installer will be kicked off by the main dSIPRouter installer by passing the MySQL DB root username, database name, and/or the root password
 # This is needed since we are installing stored procedures which require SUPER privileges on MySQL
