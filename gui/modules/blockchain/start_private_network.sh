42775499ac6ff3237de21e834abe362c681c8bac
root <root@dsiprouter-dev.localdomain>
2018-07-31 13:43:56 +0000

    Remove old files

--- gui/modules/blockchain/start_private_network.sh
+++
@@ -1,3 +0,0 @@
-DATADIR=/root/gethDataDir
-cd go-ethereum
-./build/bin/geth --fast --cache 512 --ipcpath /root/Library/Ethereum/geth.ipc --networkid 12345 --datadir $DATADIR --targetgaslimit '9000000000000' console
