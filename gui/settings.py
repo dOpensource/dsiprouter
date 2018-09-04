0df96f063ed8583a16434c58a09761d65ab1aa45
root <root@kamailio2.lhsip.com>
2018-09-04 04:40:53 -0400

    Added support for Roles.  Now a dSIPRouter instance can have a Role in the tolopology

--- gui/settings.py
+++ gui/settings.py
@@ -4,25 +4,20 @@
 DSIP_HOST = '0.0.0.0'
 DSIP_PORT = 5000
 USERNAME = 'admin'
-PASSWORD = 'admin'
-
-# ssl key / cert paths
-SSL_KEY = ""
-SSL_CERT = ""
+PASSWORD = 'NGVjYjYwYzJhMjdk'
 
 # dSIPRouter internal settings
 
 VERSION = 0.51
-DEBUG = False
+DEBUG = 0
+# '' = default behavior - handle inbound with domain mapping from endpoints, inbound from carriers and outbound to carriers
+# outbound = act as an outbound proxy only 
+ROLE = ''  
 
 # MySQL settings for kamailio
-# Supports multiple DB connection failover:
-# Specify a list of DB hostnames or ip's to enable, i.e.)
-# KAM_DB_HOST = ['db1', 'db2', 'db3']
-# kamailio.cfg must be manually configured && kamailio restarted
 
 KAM_DB_TYPE = 'mysql'
-KAM_DB_HOST = 'localhost'
+KAM_DB_HOST = ['64.129.84.40','64.129.84.41','50.237.20.140','50.237.20.141']
 KAM_DB_PORT = '3306'
 KAM_DB_NAME = 'kamailio'
 KAM_DB_USER = 'kamailio'
@@ -32,7 +27,7 @@ KAM_KAMCMD_PATH = '/usr/sbin/kamcmd'
 KAM_CFG_PATH = '/etc/kamailio/kamailio.cfg'
 
 
-# SQLAlchemy Settings
+#SQLAlchemy Settings
 
 # Will disable modification tracking
 SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -42,28 +37,22 @@ FLT_CARRIER = 8
 FLT_PBX = 9
 
 # The domain used to create user accounts for PBX and Endpoint registrations
-# defaults to this domain if not provided with username
+
 DOMAIN = 'sip.dsiprouter.org'
 
 # Teleblock Settings
-
 TELEBLOCK_GW_ENABLED = 0
-TELEBLOCK_GW_IP = '66.203.90.197'
+TELEBLOCK_GW_IP = '62.34.24.22'
 TELEBLOCK_GW_PORT = '5066'
 TELEBLOCK_MEDIA_IP = ''
 TELEBLOCK_MEDIA_PORT = ''
 
 
-# Flowroute Settings
-
-# auth info for flowroute accnt
-FLOWROUTE_ACCESS_KEY = ""
-FLOWROUTE_SECRET_KEY = ""
-FLOWROUTE_API_ROOT_URL = "https://api.flowroute.com/v2"
-
-# Network Settings
+FLOWROUTE_ACCESS_KEY=''
+FLOWROUTE_SECRET_KEY=''
+FLOWROUTE_API_ROOT_URL=''
 
 # updated dynamically! ONLY set here if you need static values
-INTERNAL_IP_ADDR = ""
-INTERNAL_IP_NET = ""
-EXTERNAL_IP_ADDR = ""
+INTERNAL_IP_ADDR = '64.129.84.43'
+INTERNAL_IP_NET = '64.129.84.*'
+EXTERNAL_IP_ADDR = '64.129.84.43'
