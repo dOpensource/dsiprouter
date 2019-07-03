f0b772396303c122ef9ebaad2b4c17d34f15f2b2
Tyler Moore <tmoore@goflyball.com>
2019-07-02 21:02:21 -0400

    Inbound DID Mapping Through API
      Resolves #77
      - create new API DID Mapping endpoint for GET,POST,PUT,DELETE
      - add tests to API test `19.sh`
      - fix creation of duplicate inbound DID's
      - update exception handling to be more useful
      - update globals
      - add TODO comments
      - update rowToDict function
      - make `error.html` prettier / more palatable

--- gui/globals.py
+++ gui/globals.py
@@ -1,3 +1,14 @@
+#
+# TODO: this module shadows globals() builtin, which is a bad practice
+# and may have unintended consequences elsewhere, we need to find a better implementation
+#
+# Also, we lose the state of global required if dsiprouter restarts
+# which means kamailio may not have reloaded memory at all
+# We should track the state of reloads in a file or database (the latter preferably)
+#
+
 def initialize():
-    global reload_required
-    reload_required = False
+    globals()['reload_required'] = False
+
+# allow references in code before initialize is called
+reload_required = globals()['reload_required'] if 'reload_required' in globals() else False
--- testing/19.sh
+++ testing/19.sh
@@ -14,6 +14,7 @@ host=$(getConfigAttrib 'DSIP_HOST' $project_dir/gui/settings.py)
 port=$(getConfigAttrib 'DSIP_PORT' $project_dir/gui/settings.py)
 username=$(getConfigAttrib 'USERNAME' $project_dir/gui/settings.py)
 password=$(getConfigAttrib 'PASSWORD' $project_dir/gui/settings.py)
+inbound_flag=$(getConfigAttrib 'FLT_INBOUND' $project_dir/gui/settings.py)
 # if dsip is bound to all available addresses use localhost
 [ "$host" = "0.0.0.0" ] && host="localhost"
 
@@ -52,12 +53,98 @@ validate() {
     status=$(curl -X GET -s --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/kamailio/stats" -o /dev/null)
     [ ${status:-400} -ne 200 ] && return 1
 
-    # cleanup, remove cookie
-    rm -f $cookie_file
+    # create entries for testing /api/v1/mapping endpoint
+    prefix0='123456789'
+    prefix1='987654321'
+    prefix2='01234'
+    prefix3='56789'
+    mysql kamailio -e "insert into dr_rules values (null,'$inbound_flag','$prefix0','',0,'','66,67','Test DID Mapping 1');"
+    mysql kamailio -e "insert into dr_rules values (null,'$inbound_flag','$prefix1','',0,'','66','Test DID Mapping 2');"
+    ruleid0=$(mysql kamailio -sA -e "select ruleid from dr_rules where groupid='9000' limit 1;")
+
+
+    # ===================
+    # GET /api/v1/mapping
+    # ===================
+    # valid requests
+    [ $(curl -s -X GET --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping" -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X GET --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping?ruleid=${ruleid0}" -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X GET --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping?did='"${prefix1}"'" -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X GET --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping?ruleid=1000000" -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X GET --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping?did=1000000" -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X GET --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping?ruleid=abcdef" -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X GET --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping?did=abcdef" -o /dev/null) -ne 200 ] && return 1
+    # invalid requests
+    [ $(curl -s -X GET --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping?doesntexist=123" -o /dev/null) -eq 200 ] && return 1
+
+    # ====================
+    # POST /api/v1/mapping
+    # ====================
+    # valid requests
+    [ $(curl -s -X POST --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping" \
+        -d '{"did": "'"${prefix2}"'", "servers": ["66","67"], "notes": "'"${prefix2}"' DID Mapping"}' -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X POST --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping" \
+        -d '{"did": "'"${prefix3}"'","servers": ["66","67"]}' -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X POST --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping" \
+        -d '{"did": "", "servers": ["66"], "notes": "Default DID Mapping"}' -o /dev/null) -ne 200 ] && return 1
+    # invalid requests
+    [ $(curl -s -X POST --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping" \
+        -d '{"servers": ["66","67"], "notes": "'"${prefix2}"' DID Mapping"}' -o /dev/null) -eq 200 ] && return 1
+    [ $(curl -s -X POST --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping" \
+        -d '{"did": "0", "servers": ["66","67","68","69","70","71","71"], "notes": "0 DID Mapping"}' -o /dev/null) -eq 200 ] && return 1
+    [ $(curl -s -X POST --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping" \
+        -d '{"did": "00", "servers": ["",""], "notes": "00 DID Mapping"}' -o /dev/null) -eq 200 ] && return 1
+    [ $(curl -s -X POST --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping" \
+        -d '{"did": "000", "servers": ["abc","efg"], "notes": "000 DID Mapping"}' -o /dev/null) -eq 200 ] && return 1
+    [ $(curl -s -X POST --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping" \
+        -d '{"did": "0000", "servers": [], "notes": "0000 DID Mapping"}' -o /dev/null) -eq 200 ] && return 1
+    [ $(curl -s -X POST --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping" \
+        -d '{"did": "00000", "notes": "00000 DID Mapping"}' -o /dev/null) -eq 200 ] && return 1
+
+    # ===================
+    # PUT /api/v1/mapping
+    # ===================
+    # valid requests
+    [ $(curl -s -X PUT --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping?ruleid=${ruleid0}" \
+        -d '{"did": "01234", "notes": "01234 DID Mapping"}' -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X PUT --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping?did='"${prefix1}"'" \
+        -d '{"servers": ["67"]}' -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X PUT --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping?did=10000000" \
+        -d '{"did": "01234", "notes": "01234 DID Mapping"}' -o /dev/null) -ne 200 ] && return 1
+    # invalid requests
+    [ $(curl -s -X PUT --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping?doesntexist=123" \
+        -d '{"notes": "New DID Mapping"}' -o /dev/null) -eq 200 ] && return 1
+    [ $(curl -s -X PUT --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping" \
+        -d '{"notes": "Newer DID Mapping"}' -o /dev/null) -eq 200 ] && return 1
+    [ $(curl -s -X PUT --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping?did='"${prefix1}"'" \
+        -d '{}' -o /dev/null) -eq 200 ] && return 1
+    [ $(curl -s -X PUT --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping?did=01234" \
+        -d '{"doesntexist": "2"}' -o /dev/null) -eq 200 ] && return 1
+    [ $(curl -s -X PUT --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" --connect-timeout 3 -H "Content-Type: application/json" "$base_url/api/v1/mapping?did=01234" \
+        -d '{"doesntexist": "2", "notes": "New DID Mapping"}' -o /dev/null) -eq 200 ] && return 1
+
+    # ======================
+    # DELETE /api/v1/mapping
+    # ======================
+    # valid requests
+    [ $(curl -s -X DELETE --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping?ruleid=${ruleid0}" -o /dev/null) -ne 200 ] && return 1
+    [ $(curl -s -X DELETE --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping?did='"${prefix1}"'" -o /dev/null) -ne 200 ] && return 1
+    # invalid requests
+    [ $(curl -s -X DELETE --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping?doesntexist=123" -o /dev/null) -eq 200 ] && return 1
+    [ $(curl -s -X DELETE --connect-timeout 3 -H "Authorization: Bearer ${token}" -w "%{http_code}" "$base_url/api/v1/mapping" -o /dev/null) -eq 200 ] && return 1
+
 
     # if we made it this far all checks passed
     return 0
 }
+
+# cleanup, remove cookie, remove DB entries
+cleanupHandler() {
+    rm -f $cookie_file
+    mysql kamailio -e "delete from dr_rules where groupid='$inbound_flag';"
+}
+
+trap cleanupHandler EXIT
 
 validate; ret=$?
 
