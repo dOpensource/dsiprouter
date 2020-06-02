87b771ec8ea8ec5fcbf08c24fcbf2c34ae76db9f
root <root@sbc.dsiprouter.net>
2020-06-02 21:40:30 +0000

    Certificates Module - Added SQL to store the certs in the DB - Added the standard install script

--- gui/modules/certificates/certificates.sql
+++ gui/modules/certificates/certificates.sql
@@ -1,70 +1,9 @@
--- MySQL dump 10.16  Distrib 10.1.26-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: kamailio
--- ------------------------------------------------------
--- Server version	10.1.26-MariaDB-0+deb9u1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `dsip_multidomain_mapping`
---
-
-DROP TABLE IF EXISTS `dsip_multidomain_mapping`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `dsip_multidomain_mapping` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `pbx_id` int(10) NOT NULL,
-  `db_host` varchar(20) NOT NULL,
-  `db_username` varchar(40) NOT NULL,
-  `db_password` varchar(40) NOT NULL,
-  `domain_list` varchar(255) NOT NULL DEFAULT '',
-  `domain_list_hash` varchar(255) NOT NULL DEFAULT '',
-  `attr_list` varchar(255) NOT NULL DEFAULT '',
-  `type` tinyint(3) NOT NULL DEFAULT '0',
-  `enabled` tinyint(1) NOT NULL DEFAULT '0',
-  `lastsync` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `syncstatus` tinyint(1) NOT NULL DEFAULT '0',
-  `syncerror` varchar(200) NOT NULL DEFAULT '',
+DROP TABLE IF EXISTS `dsip_certificates`; 
+CREATE TABLE IF NOT EXISTS `dsip_certificates` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `domain` VARCHAR(128) NULL,
+  `type` VARCHAR(45) NULL,
+  `cert` BLOB NULL,
+  `key` BLOB NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `dsip_domain_mapping`
---
-
-DROP TABLE IF EXISTS `dsip_domain_mapping`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `dsip_domain_mapping` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `pbx_id` int(10) NOT NULL,
-  `domain_id` int(10) NOT NULL,
-  `attr_list` varchar(255) NOT NULL,
-  `type` tinyint(3) NOT NULL DEFAULT '0',
-  `enabled` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2018-08-28 22:04:51
+);
