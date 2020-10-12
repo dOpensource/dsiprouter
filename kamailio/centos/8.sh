b89473a34ba4911150e72d636e7660763c6eb62d
Richard Bolaji <56362787+RichSosa28@users.noreply.github.com>
2020-10-12 09:43:38 -0400

    Update 8.sh

--- kamailio/centos/8.sh
+++ kamailio/centos/8.sh
@@ -10,7 +10,7 @@ function install() {
     dnf install -y psmisc curl wget sed gawk vim epel-release perl firewalld libuuid-devel openssl-devel
     dnf install -y logrotate rsyslog
 
-    dnf install -y mariadb mariadb-devel mariadb-server
+    #dnf install -y mariadb mariadb-devel mariadb-server
     ln -s /usr/share/mariadb/ /usr/share/mysql
     # Make sure no extra configs present on fresh install
     rm -f ~/.my.cnf
@@ -88,13 +88,13 @@ EOF
         kamailio-http_async_client kamailio-dmq_userloc
 
     # workaround for kamailio rpm transaction failures
-    if (( $? != 0 )); then
-        rpm --import $(grep 'gpgkey' /etc/dnf.repos.d/kamailio.repo | cut -d '=' -f 2)
-        REPOS='kamailio kamailio-ldap kamailio-mysql kamailio-postgresql kamailio-debuginfo kamailio-xmpp kamailio-unixodbc kamailio-utils kamailio-tls kamailio-presence kamailio-outbound kamailio-gzcompress'
-        for REPO in $REPOS; do
-            dnf install -y $(grep 'baseurl' /etc/dnf.repos.d/kamailio.repo | cut -d '=' -f 2)$(uname -m)/$(repoquery -i ${REPO} | head -4 | tail -n 3 | tr -d '[:blank:]' | cut -d ':' -f 2 | perl -pe 'chomp if eof' | tr '\n' '-').$(uname -m).rpm
-        done
-    fi
+    #if (( $? != 0 )); then
+     #   rpm --import $(grep 'gpgkey' /etc/dnf.repos.d/kamailio.repo | cut -d '=' -f 2)
+      #  REPOS='kamailio kamailio-ldap kamailio-mysql kamailio-postgresql kamailio-debuginfo kamailio-xmpp kamailio-unixodbc kamailio-utils kamailio-tls kamailio-presence kamailio-outbound kamailio-gzcompress'
+       # for REPO in $REPOS; do
+        #    dnf install -y $(grep 'baseurl' /etc/dnf.repos.d/kamailio.repo | cut -d '=' -f 2)$(uname -m)/$(repoquery -i ${REPO} | head -4 | tail -n 3 | tr -d '[:blank:]' | cut -d ':' -f 2 | perl -pe 'chomp if eof' | tr '\n' '-').$(uname -m).rpm
+        #done
+    #fi
 
     # get info about the kamailio install for later use in script
     KAM_VERSION_FULL=$(kamailio -v 2>/dev/null | grep '^version:' | awk '{print $3}')
