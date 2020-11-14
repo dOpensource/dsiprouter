302a6275995e2398f18aa59ad871554456d4b68c
root <root@nightly-centos7.dsiprouter.org>
2020-11-14 21:29:37 +0000

    Changed the order in which firewall rules are applied first and then firewalld is started

--- kamailio/centos/7.sh
+++ kamailio/centos/7.sh
@@ -148,6 +148,14 @@ EOF
 
     # Execute 'kamdbctl create' to create the Kamailio database schema
     kamdbctl create
+    
+    # Setup firewall rules
+    firewall-offline-cmd --zone=public --add-port=${KAM_SIP_PORT}/udp --permanent
+    firewall-offline-cmd --zone=public --add-port=${KAM_SIP_PORT}/tcp --permanent
+    firewall-offline-cmd --zone=public --add-port=${KAM_SIPS_PORT}/tcp --permanent
+    firewall-offline-cmd --zone=public --add-port=${KAM_WSS_PORT}/tcp --permanent
+    firewall-offline-cmd --zone=public --add-port=${KAM_DMQ_PORT}/udp --permanent
+    firewall-offline-cmd --zone=public --add-port=${RTP_PORT_MIN}-${RTP_PORT_MAX}/udp
 
     # Start firewalld
     systemctl start firewalld
@@ -159,14 +167,7 @@ EOF
         systemctl restart firewalld
     fi
 
-    # Setup firewall rules
-    firewall-cmd --zone=public --add-port=${KAM_SIP_PORT}/udp --permanent
-    firewall-cmd --zone=public --add-port=${KAM_SIP_PORT}/tcp --permanent
-    firewall-cmd --zone=public --add-port=${KAM_SIPS_PORT}/tcp --permanent
-    firewall-cmd --zone=public --add-port=${KAM_WSS_PORT}/tcp --permanent
-    firewall-cmd --zone=public --add-port=${KAM_DMQ_PORT}/udp --permanent
-    firewall-cmd --zone=public --add-port=${RTP_PORT_MIN}-${RTP_PORT_MAX}/udp
-    firewall-cmd --reload
+
 
     # Make sure MariaDB and Local DNS start before Kamailio
     if ! grep -v 'mysql.service dnsmasq.service' /lib/systemd/system/kamailio.service; then
