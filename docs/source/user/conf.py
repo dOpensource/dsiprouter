d585223c6a69512f4cb56d522d460296599a69e6
Mack Hendricks <mack@dopensource.com>
2020-12-04 08:28:36 -0500

    Update conf.py

--- docs/source/user/conf.py
+++ docs/source/user/conf.py
@@ -16,7 +16,7 @@ import os
 import sys
 #sys.path.insert(0, os.path.abspath('../../..'))
 #sys.path.insert(0, os.path.abspath('../../gui'))
-import settings
+#import settings
 sys.setrecursionlimit(1500)
 
 # -- Project information -----------------------------------------------------
