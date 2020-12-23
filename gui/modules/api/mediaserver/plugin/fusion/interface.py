2ffa80464ecb90ee77856939114833c074327cb0
Mack Hendricks <mack@dopensource.com>
2020-12-23 06:02:58 +0000

    Media Server API - Added support for creating a domain

--- gui/modules/api/mediaserver/plugin/fusion/interface.py
+++ gui/modules/api/mediaserver/plugin/fusion/interface.py
@@ -1,5 +1,11 @@
 # FusionPBX Plugin for the Media Server api
 import psycopg2
+import psycopg2.extras
+import json
+import uuid
+
+
+
 
 class mediaserver():
 
@@ -9,52 +15,88 @@ class mediaserver():
     username=''
     password=''
     dbname="fusionpbx"
+    db=''
 
 
-    def __init__(self, hostname,username,password,dbname,port='5432',auth_type=None):
-        self.hostname = hostname
-        self.port = port
-        self.auth_type = auth_type
-        self.username = username
-        self.password = password
-        self.dbname = dbname
+    def __init__(self, config):
+        self.hostname =config.hostname
+        self.port = config.port
+        self.auth_type = config.auth_type
+        self.username = config.username
+        self.password = config.password
+        self.dbname = config.dbname
 
     def testConnection(self):
+        print("testConnection")
         pass
 
     def getConnection(self):
         try:
             db = psycopg2.connect(host=self.hostname,port=self.port,user=self.username,password=self.password,dbname=self.dbname)
             if db is not None:
-                print("Connection to FusionPBX:{} database was successful".format(self.hostname))
+                print("Connection to FusionPBX: {} database was successful".format(self.hostname))
                 return db
 
         except Exception as ex:
             raise
 
-    def getDomains():
-        return domain(mediaserver)
-
     def closeConnection(self):
-        pass
+        db.close()
 
 class domain():
+    domain_id=""
+    name = ""
+    enabled = ""
+    description = ""
+
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+            sort_keys=True, indent=4)
+
+class domains():
 
     mediaserver=''
+    db=''
+    domain_list = []
 
     def __init__(self, mediaserver):
         self.mediaserver = mediaserver
+        self.db = self.mediaserver.getConnection()
 
-    def createDomain():
+    def create(self,data):
+        # call it in any place of your program
+        # before working with UUID objects in PostgreSQL
+        psycopg2.extras.register_uuid()
+        domain_uuid = uuid.uuid4()
+        cur = self.db.cursor()
+        cur.execute("""insert into v_domains (domain_uuid,domain_name,domain_enabled,domain_description) \
+                    values (%s,%s,%s,%s)""",(domain_uuid,data['name'],data['enabled'],data['description']))
+        self.db.commit()
+        cur.close()
+
+    def getDomain(self,domain_name=None):
+        cur = self.db.cursor()
+        cur.execute("""select domain_uuid,domain_name,domain_enabled,domain_description from v_domains where domain_enabled='true'""")
+        rows = cur.fetchall()
+        if rows is not None:
+            for row in rows:
+                d = {}
+                d['domain_id'] = row[0]
+                d['name']= row[1]
+                d['enabled'] = row[2]
+                d['description'] = row[3]
+
+                self.domain_list.append(d)
+
+        return self.domain_list
+
+
+
+
+    def update():
         pass
 
-    def getDomain():
-        pass
-
-    def updateDomain():
-        pass
-
-    def deleteDomain():
+    def delete():
         pass
 
     def getExtensions():
--- gui/modules/api/mediaserver/routes.py
+++ gui/modules/api/mediaserver/routes.py
@@ -2,14 +2,83 @@ import os
 from flask import Blueprint, render_template, abort, jsonify
 from util.security import api_security
 from database import SessionLoader, DummySession, dSIPMultiDomainMapping
-from shared import showApiError,debugEndpoint,StatusCodes
+from shared import showApiError,debugEndpoint,StatusCodes, getRequestData
 from enum import Enum
+from werkzeug import exceptions as http_exceptions
 import importlib.util
 import settings, globals
 
+class config():
 
+    hostname=''
+    port=''
+    username=''
+    password=''
+    dbname=''
+    type=''
+    auth_type=''
+    plugin_type=''
+    plugin=None
+
+
+
+    def __init__(self,config_id):
+
+        db = DummySession()
+
+        db = SessionLoader()
+        try:
+            print("The config_id is {}".format(config_id))
+            domainMapping = db.query(dSIPMultiDomainMapping).filter(dSIPMultiDomainMapping.pbx_id == config_id).first()
+            if domainMapping is None:
+                raise Exception("Configuration doesn't exist")
+            else:
+                print("***In domain mapping***")
+                self.hostname=domainMapping.db_host
+                #self.port=domainMapping.port if "port" in domainMapping else "5432"
+                self.port="5432"
+                self.username=domainMapping.db_username
+                self.password=domainMapping.db_password
+                self.dbname="fusionpbx"
+
+                #response_payload['msg'] = 'Domain Configuration Exists'
+
+                if domainMapping.type == int(dSIPMultiDomainMapping.FLAGS.TYPE_FUSIONPBX.value):
+                    self.plugin_type = FLAGS.FUSIONPBX_PLUGIN
+                elif domainMapping.type == dSIPMultiDomainMapping.FLAGS.TYPE_FUSIONPBX_CLUSTER.value:
+                    self.plugin_type = FLAGS.FUSIONPBX_PLUGIN
+                elif domainMapping.type == dSIPMultiDomainMapping.FLAGS.TYPE_FREEPBX.value:
+                    self.plugin_type = FLAGS.FREEPBX_PLUGIN;
+                    raise Exception("FreePBX Plugin is not supported yet")
+                else:
+                    raise Exception("PBX plugin for config #{} can not be found".format(config_id))
+
+                # Import plugin
+
+                # Returns the Base directory of this file
+                base_dir = os.path.dirname(__file__)
+
+                # Use the Base Dir to specify the location of the plugin required for this domain
+                spec = importlib.util.spec_from_file_location("plugin.{}".format(self.plugin_type), "{}/plugin/{}/interface.py".format(base_dir,self.plugin_type))
+                self.plugin = importlib.util.module_from_spec(spec)
+                if spec.loader.exec_module(self.plugin):
+                    print("***Plugin was loaded***")
+                return
+
+        except Exception as ex:
+            raise ex
+        finally:
+            db.close()
+
+
+    def getPlugin(self):
+        if self.plugin:
+            return self.plugin
+        else:
+            raise Exception("The plugin could not be loaded")
 
 class FLAGS():
+
     FUSIONPBX_PLUGIN = "fusion"
     FREEPBX_PLUGIN = "freepbx"
 
@@ -61,80 +130,82 @@ def getDomains(config_id=None):
     # Currently we only support FusionPBX
     # Check if Configuration ID exists
 
-    db = DummySession()
-
     response_payload = {'error': '', 'msg': '', 'kamreload': globals.reload_required, 'data': []}
 
     try:
         if settings.DEBUG:
             debugEndpoint()
 
-        db = SessionLoader()
-
-        if config_id != None:
-            domainMapping = db.query(dSIPMultiDomainMapping).filter(dSIPMultiDomainMapping.pbx_id == config_id).first()
-            if (domainMapping) is not None:
-                response_payload['msg'] = 'Domain Configuration Exists'
-
-                if domainMapping.type == int(dSIPMultiDomainMapping.FLAGS.TYPE_FUSIONPBX.value):
-                    plugin = FLAGS.FUSIONPBX_PLUGIN
-                elif domainMapping.type == dSIPMultiDomainMapping.FLAGS.TYPE_FUSIONPBX_CLUSTER.value:
-                    plugin = FLAGS.FUSIONPBX_PLUGIN
-                elif domainMapping.type == dSIPMultiDomainMapping.FLAGS.TYPE_FREEPBX.value:
-                    plugin = FLAGS.FREEPBX_PLUGIN;
-                    raise Exception("FreePBX Plugin is not supported yet")
-                else:
-                    raise Exception("PBX plugin for config #{} can not be found".format(config_id))
-
-
-                # Import plugin
-
-                # Returns the Base directory of this file
-                base_dir = os.path.dirname(__file__)
-
-                # Use the Base Dir to specify the location of the plugin required for this domain
-                spec = importlib.util.spec_from_file_location(plugin, "{}/plugin/{}/interface.py".format(base_dir,plugin))
-                module = importlib.util.module_from_spec(spec)
-                spec.loader.exec_module(module)
-
+            if config_id != None:
+                config_info = config(config_id)
+                plugin = config_info.getPlugin()
                 # Create instance of Media Server Class
-                mediaserver = module.mediaserver(hostname=domainMapping.db_host,username=domainMapping.db_username, \
-                                                password=domainMapping.db_password, \
-                                                dbname="fusionpbx" \
-                                                )
-                # Get a Connection to the Media Server
-                conn = mediaserver.getConnection()
-
-                # Todo
-                # Use plugin to get list of domains by calling plugin.<pbxtype>.getDomains()
-
+                if plugin:
+                    mediaserver = plugin.mediaserver(config_info)
+                    if mediaserver:
+                        domains = plugin.domains(mediaserver)
+                        # Use plugin to get list of domains by calling plugin.<pbxtype>.getDomain()
+                        domain_list = domains.getDomain()
+                        response_payload['msg'] = '{} domains were found'.format(len(domain_list))
+                        response_payload['data'].append(domain_list)
             else:
-                response_payload['msg'] = 'No domain configuration exists for config #{}'.format(config_id)
-        else:
-            raise Exception("The configuration id must be provided")
-
-
-        #db.commit()
+                raise Exception("The configuration id must be provided")
 
         return jsonify(response_payload), StatusCodes.HTTP_OK
 
     except Exception as ex:
-        db.rollback()
-        db.flush()
         return showApiError(ex)
-    finally:
-        db.close()
+
 
 
 
 @mediaserver.route('/api/v1/mediaserver/domain',methods=['POST'])
 @api_security
 def postDomains():
-    result = "{ \
-        domain_id: 1012 \
-        name: AprilandMackCo, \
-        enabled: true, \
-        description: 'April and Mack Co', \
-        config_id: 64 \
-        }"
-    return result;
+
+    # use a whitelist to avoid possible buffer overflow vulns or crashes
+    VALID_REQUEST_DATA_ARGS = {"name": str, "enabled": bool, "description": str, "config_id": int}
+
+    # ensure requred args are provided
+    REQUIRED_ARGS = {'name','config_id'}
+
+    # defaults.. keep data returned separate from returned metadata
+    response_payload = {'error': '', 'msg': '', 'kamreload': globals.reload_required, 'data': []}
+
+
+    try:
+        if (settings.DEBUG):
+            debugEndpoint()
+
+
+        # get request data
+        data = getRequestData()
+
+        # sanity checks
+        for k, v in data.items():
+            if k not in VALID_REQUEST_DATA_ARGS.keys():
+                raise http_exceptions.BadRequest("Request data argument '{}' not recognized".format(k))
+            if not type(v) == VALID_REQUEST_DATA_ARGS[k]:
+                raise http_exceptions.BadRequest("Request data argument '{}' not valid".format(k))
+
+        for k in REQUIRED_ARGS:
+            if k not in VALID_REQUEST_DATA_ARGS.keys():
+                raise http_exceptions.BadRequest("Request argument '{}' is required".format(k))
+
+        config_id = data['config_id']
+
+        # Create instance of Media Server Class
+        if config_id != None:
+            config_info = config(config_id)
+            plugin = config_info.getPlugin()
+            # Create instance of Media Server Class
+            if plugin:
+                mediaserver = plugin.mediaserver(config_info)
+                if mediaserver:
+                    domains = plugin.domains(mediaserver)
+                    domains.create(data)
+
+        return jsonify(response_payload), StatusCodes.HTTP_OK
+
+    except Exception as ex:
+        return showApiError(ex)
