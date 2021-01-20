a5ecbd575c741b310e885f2c83e4027a934e6f64
Mack Hendricks <mack@dopensource.com>
2021-01-20 12:19:11 +0000

    Fixed issue with settings.py not updating correctly

--- gui/util/security.py
+++ gui/util/security.py
@@ -1,3 +1,7 @@
+# make sure the generated source files are imported instead of the template ones
+import sys
+sys.path.insert(0, '/etc/dsiprouter/gui')
+
 import os, hashlib, binascii, string, ssl, OpenSSL
 from Crypto.Cipher import AES
 from Crypto.Util import Counter
