6e25bc7cd582d62cfa688c2203be577804624435
Maurice Rogers <cruzer45@gmail.com>
2022-09-09 22:14:33 -0600

    Removed unused imports that were preventing the app from starting

--- gui/modules/api/auth/functions.py
+++ gui/modules/api/auth/functions.py
@@ -1,6 +1,5 @@
-from shared import getInternalIP, getExternalIP, updateConfig, getCustomRoutes, debugException, debugEndpoint, \
-    stripDictVals, strFieldsToDict, dictToStrFields, allowed_file, showError, hostToIP, IO, objToDict, StatusCodes, \
-    safeUriToHost, safeFormatSipUri, safeStripPort
+from shared import  updateConfig, getCustomRoutes, debugException, debugEndpoint, \
+    stripDictVals, strFieldsToDict, dictToStrFields, allowed_file, showError,  IO, objToDict, StatusCodes
 from database import db_engine, SessionLoader, DummySession, Gateways, Address, InboundMapping, OutboundRoutes, \
     Subscribers, \
     dSIPLCR, UAC, GatewayGroups, Domain, DomainAttrs, dSIPDomainMapping, dSIPMultiDomainMapping, Dispatcher, \
